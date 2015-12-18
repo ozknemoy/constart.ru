01ebcced27b1141de5cde27044f0a6401545e0d9
ozknemoy <ozk.nemoy@mail.ru>
2015-12-18 15:04:19 +0300

    Delete test.js

--- Content/test.js
+++
@@ -1,4 +0,0 @@
-﻿window.onload=function(){img=document.getElementById('img'),width=img.clientWidth,height=img.clientHeight,canvas=document.getElementById('slow'),ctx=canvas.getContext('2d');canvas.width=width;canvas.height=height;ctx.drawImage(img,0,0);}
-var value=0,br=document.getElementById('br'),co=document.getElementById('co');br.addEventListener('change',update);co.addEventListener('change',update);function update(){valueBr=parseInt(br.value);valueCo=parseInt(co.value);commonF(img,ctx,width,height,valueBr,valueCo);}
-function commonF(img,ctx,w,h,valueBr,valueCo){ctx.drawImage(img,0,0);var imageData=ctx.getImageData(0,0,w,h),d=imageData.data;var factor=(259*(valueCo+255))/(255*(259-valueCo));for(var i=0,l=d.length;i<l;i+=4){d[i]+=valueBr;d[i+1]+=valueBr;d[i+2]+=valueBr;d[i]=factor*(d[i]-128)+128;d[i+1]=factor*(d[i+1]-128)+128;d[i+2]=factor*(d[i+2]-128)+128;}
-ctx.putImageData(imageData,0,0);}
